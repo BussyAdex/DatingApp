19161037d796ff5469b0ad9ac04f8ebfc34fcb65
Busyadex <Bussyadex42@gmail.com>
2024-12-24 01:34:46 +0000

    End of Session 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,8 +11,8 @@ import { AccountService } from '../_services/account.service';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  private accountService = inject(AccountService)
-  // usersFromHomeComponent = input.required<any>();
+  private accountService = inject(AccountService);
+  private toastr = inject(ToastrService)
   cancelRegister = output<boolean>();
   model: any = {}
 
@@ -21,7 +22,11 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => {
+        this.toastr.error(error.error);
+        console.log(error)
+      }
+
     })
   }
 
