59ec5ee8bacdab54cad89a8fc30cfed3702b144c
Busyadex <Bussyadex42@gmail.com>
2025-01-03 00:23:38 +0000

    End of Session 10

--- client/src/app/_guards/auth.guard.ts
+++ client/src/app/_guards/auth.guard.ts
@@ -5,13 +5,12 @@ import { ToastrService } from 'ngx-toastr';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const accountService = inject(AccountService);
-  const toastr = inject(ToastrService)
+  const toastr = inject(ToastrService);
 
   if (accountService.currentUser()) {
     return true;
   } else {
-    toastr.error('You shall not pass');
+    toastr.error('You shall not pass!');
     return false;
   }
-  return true;
 };
